881a2bc3e2845a24c15b6899ee4627220e9dbc01
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-02 15:26:17 +0200

    add print numbers

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log("prova");
+// ciclo di controllo per i numeri divisibili per tre e/o cinque 
+
+for (let i = 1; i<=100; i++) {
+  if (((i % 3) === 0) && ((i % 5) === 0)) {
+
+    console.log("FizBuzz");
+
+  } else if((i % 3) === 0) {
+
+    console.log("Fizz");
+
+  } else if ((i % 5) === 0) {
+
+    console.log("Buzz")
+
+  } else {
+
+    console.log(i);
+    
+  }
+}
