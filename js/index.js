21f549fb73655e7b40e0ba3283ff2cd6b21ed319
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-02 16:26:15 +0200

    add DOM container and style

--- js/index.js
+++ js/index.js
@@ -1,21 +1,33 @@
 // ciclo di controllo per i numeri divisibili per tre e/o cinque 
 
-for (let i = 1; i<=100; i++) {
-  if (((i % 3) === 0) && ((i % 5) === 0)) {
+  for (let i = 1; i<=100; i++) {
 
-    console.log("FizBuzz");
+    const node = document.createElement("div");
 
-  } else if((i % 3) === 0) {
+    if (((i % 3) === 0) && ((i % 5) === 0)) {
 
-    console.log("Fizz");
+       // Create a text node:
+      let textnode = document.createTextNode("FizzBuzz");
+      node.appendChild(textnode);
+      document.getElementById("container").appendChild(node).classList.add('counter', 'three-five-multy');
+  
+    } else if((i % 3) === 0) {
+  
+      let textnode = document.createTextNode("Fizz");
+      node.appendChild(textnode);
+      document.getElementById("container").appendChild(node).classList.add('counter', 'three-multy');
+  
+    } else if ((i % 5) === 0) {
+  
+      let textnode = document.createTextNode("Buzz");
+      node.appendChild(textnode);
+      document.getElementById("container").appendChild(node).classList.add('counter', 'five-multy');
+  
+    } else {
+  
+      let textnode = document.createTextNode(i);
+      node.appendChild(textnode);
+      document.getElementById("container").appendChild(node).classList.add('counter', 'other-number');
+    }
 
-  } else if ((i % 5) === 0) {
-
-    console.log("Buzz")
-
-  } else {
-
-    console.log(i);
-    
   }
-}
